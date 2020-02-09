31109e14787640f2c820a1e4114475d06f276de6
Michał Kalbarczyk <fazibear@gmail.com>
2020-02-09 16:15:23 +0100

    Disable server in iex

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,6 @@ config :night_game, NightGameWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  server: true,
   watchers: [
     node: [
       "node_modules/webpack/bin/webpack.js",
