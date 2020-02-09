2c8b13661bf2070156fa14d64f4c2529852fb02d
Michał Kalbarczyk <fazibear@gmail.com>
2020-02-09 16:14:17 +0100

    Extract HeroNameGenerator

---
+++ lib/night_game/hero_name_generator.ex
@@ -0,0 +1,92 @@
+defmodule NightGame.HeroNameGenerator do
+  @moduledoc """
+  Generate random name for you hero
+  """
+
+  @names ~w[
+    Reid
+    Bryant
+    Boyce
+    Max
+    Clair
+    Denis
+    Sung
+    Tory
+    Dannie
+    Santiago
+    Adolfo
+    Haywood
+    Robert
+    Andy
+    Horace
+    Alberto
+    Ruben
+    Vernon
+    Royal
+    Teodoro
+    Dana
+    Charlie
+    Vincenzo
+    Chi
+    Erik
+    Noel
+    Russel
+    Jonas
+    Rocco
+    Guillermo
+    Les
+    Clinton
+    Cornell
+    Chung
+    Ty
+    Arnold
+    Anton
+    Octavio
+    Logan
+    Lavern
+    Luke
+    Chong
+    Greg
+    Marty
+    Sal
+    Elmer
+    Garrett
+    Chester
+    Adalberto
+    Darrel
+    Kami
+    Mickie
+    Sigrid
+    Donita
+    Jen
+    Aimee
+    Klara
+    Else
+    Candis
+    Yajaira
+    Delphia
+    Elli
+    Rebbeca
+    Patrina
+    Marcella
+    Cleotilde
+    Velvet
+    Pei
+    Aleida
+    Shelba
+    Eneida
+    Tawanda
+    Mertie
+    Katlyn
+    Inga
+    Maxine
+    Diann
+    Nakia
+    Charlotte
+    Lauri
+  ]
+
+  def random do
+    Enum.random(@names)
+  end
+end
--- lib/night_game_web/controllers/page_controller.ex
+++ lib/night_game_web/controllers/page_controller.ex
@@ -1,90 +1,9 @@
 defmodule NightGameWeb.PageController do
   use NightGameWeb, :controller
 
-  @names ~w[
-    Reid
-    Bryant
-    Boyce
-    Max
-    Clair
-    Denis
-    Sung
-    Tory
-    Dannie
-    Santiago
-    Adolfo
-    Haywood
-    Robert
-    Andy
-    Horace
-    Alberto
-    Ruben
-    Vernon
-    Royal
-    Teodoro
-    Dana
-    Charlie
-    Vincenzo
-    Chi
-    Erik
-    Noel
-    Russel
-    Jonas
-    Rocco
-    Guillermo
-    Les
-    Clinton
-    Cornell
-    Chung
-    Ty
-    Arnold
-    Anton
-    Octavio
-    Logan
-    Lavern
-    Luke
-    Chong
-    Greg
-    Marty
-    Sal
-    Elmer
-    Garrett
-    Chester
-    Adalberto
-    Darrel
-    Kami
-    Mickie
-    Sigrid
-    Donita
-    Jen
-    Aimee
-    Klara
-    Else
-    Candis
-    Yajaira
-    Delphia
-    Elli
-    Rebbeca
-    Patrina
-    Marcella
-    Cleotilde
-    Velvet
-    Pei
-    Aleida
-    Shelba
-    Eneida
-    Tawanda
-    Mertie
-    Katlyn
-    Inga
-    Maxine
-    Diann
-    Nakia
-    Charlotte
-    Lauri
-  ]
+  alias NightGame.HeroNameGenerator
 
   def index(conn, _params) do
-    redirect(conn, to: "/game/#{Enum.random(@names)}")
+    redirect(conn, to: "/game/#{HeroNameGenerator.random()}")
   end
 end
