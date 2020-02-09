0a23334d4482448427e24d93d82b2401499b13c8
Michał Kalbarczyk <fazibear@gmail.com>
2020-02-09 13:29:58 +0100

    Change host for production release

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :night_game, NightGameWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "nightgame.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
