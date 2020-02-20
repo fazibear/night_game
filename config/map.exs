ddce0a87836748e987a937c8f7eb9dd509624b41
Michał Kalbarczyk <fazibear@gmail.com>
2020-02-20 11:05:57 +0100

    Move map to config file

---
+++ config/map.exs
@@ -0,0 +1,33 @@
+use Mix.Config
+
+config :night_game, World, ~w[
+  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall 
+  wall grass grass grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass grass grass grass grass grass  wall 
+  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass grass  wall grass grass  wall  wall 
+  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass  wall 
+  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass  wall 
+  wall grass grass grass grass grass grass  wall grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass  wall 
+  wall  wall  wall  wall  wall grass grass  wall grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass  wall  wall grass grass grass grass grass grass  wall  wall  wall grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass  wall grass grass  wall grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass  wall  wall  wall  wall grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall  wall  wall  wall  wall grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall  wall grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall 
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall 
+  wall grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall  
+  wall grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass grass  wall grass grass grass grass grass grass  wall 
+  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  wall  
+]
